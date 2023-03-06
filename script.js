932a42a140003b9cc16239b518fd3b4622d5be9c
simonakosrobert <126769620+simonakosrobert@users.noreply.github.com>
2023-03-06 20:32:22 +0100

    updated styling

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ function Buy_one(){
     if (current_workers < 1 & current_money >= 10){
         buy_one_timed()
         timer = setInterval(buy_one_timed, buy_one_time);
+        document.getElementById("myprogressBar").style.backgroundColor = "#7CFC00"
     }
 
     if (current_money >= 10) {
@@ -19,20 +20,21 @@ function Buy_one(){
     }
 
     function buy_one_timed(){
-        current_money += current_workers;
-        update_money();
-
+        
         var element = document.getElementById("myprogressBar");
         var width = 1;
         var identity = setInterval(scene, 10);
         function scene() {
         if (width >= 100) {
             clearInterval(identity);
+            current_money += current_workers;
+            update_money();
         } else {
             width++; 
             element.style.width = width + '%';
         }    
         } 
+        
     }
 }
 
@@ -46,8 +48,7 @@ function update_workers(){
 
 function update() {
 
-    current_money += click_money;   
-    update_money(click_money);
+    
     var element = document.getElementById("myprogressBar");
     var width = 1;
     var identity = setInterval(scene, 10);
@@ -55,10 +56,20 @@ function update() {
     if (width >= 100) {
         clearInterval(identity);
         document.querySelector('#click_button').disabled = false;
+        document.getElementById("myprogressBar").style.backgroundColor = "#DC143C"
+
+        // document.getElementById('click_button').style.backgroundColor = "#1899D6"
+
+        current_money += click_money;   
+        update_money(click_money);
     } else {
         width++; 
         element.style.width = width + '%';
+        // document.getElementById('click_button').style.backgroundColor = "#DC143C"
+
+        document.getElementById("myprogressBar").style.backgroundColor = "#7CFC00"
         document.querySelector('#click_button').disabled = true;              
     }    
   }
+    
 }
